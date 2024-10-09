537b3f23a99619a83ae7c8aa7dc954f60b6d5b6e
Tai Keith <keithsteve.ks@hotmail.com>
2024-10-09 23:51:02 +0000

    Created functions specific to employees

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -51,7 +51,23 @@ function createEmployee(salary: number | string): Director | Teacher {
   }
 }
 
+// Type predicate to check if an employee is a Director
+function isDirector(employee: Director | Teacher): employee is Director {
+  return (employee as Director).workDirectorTasks !== undefined;
+}
+
+// Function to execute work based on the employee type
+function executeWork(employee: Director | Teacher): void {
+  if (isDirector(employee)) {
+    console.log(employee.workDirectorTasks());
+  } else {
+    console.log(employee.workTeacherTasks());
+  }
+}
 
 console.log(createEmployee(200));
 console.log(createEmployee(1000));
 console.log(createEmployee('$500'));
+
+executeWork(createEmployee(200));
+executeWork(createEmployee(1000));
