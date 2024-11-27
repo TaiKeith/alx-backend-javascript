a3565059757ea1f6ff19a0ff8ef41ede080db8ba
Tai Keith <keithsteve.ks@hotmail.com>
2024-11-27 14:21:28 +0000

    Fixed code

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -1,39 +1,31 @@
 #!/usr/bin/env node
-
 const express = require('express');
-const countStudents = require('./3-read_file_async');
+const students = require('./3-read_file_async');
 
 const app = express();
-const PORT = 1245;
+const hostname = '127.0.0.1';
+const port = 1245;
 
-// Define the root route
 app.get('/', (req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
   res.send('Hello Holberton School!');
 });
 
-// Define the /students route
 app.get('/students', async (req, res) => {
-  const databasePath = process.argv[2];
-
+  res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.write('This is the list of our students\n');
-
-  try {
-    await countStudents(databasePath)
-      .then(() => {
-        res.end();
-      })
-      .catch(() => {
-        res.end('Cannot load the database\n');
-      });
-  } catch (error) {
-    res.end('Cannot load the database\n');
-  }
+  await students(process.argv[2]).then((data) => {
+    res.write(`Number of students: ${data.students.length}\n`);
+    res.write(`Number of students in CS: ${data.csStudents.length}. List: ${data.csStudents.join(', ')}\n`);
+    res.write(`Number of students in SWE: ${data.sweStudents.length}. List: ${data.sweStudents.join(', ')}`);
+  }).catch((err) => res.write(err.message))
+    .finally(() => {
+      res.end();
+    });
 });
 
-
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+app.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}`);
 });
-
-module.exports = app;
