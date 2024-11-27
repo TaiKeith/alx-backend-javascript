c3bc8fa0066b242ee7af75ac20df2505ff7fe45a
Tai Keith <keithsteve.ks@hotmail.com>
2024-11-27 14:14:05 +0000

    Fixed

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,41 +1,34 @@
 #!/usr/bin/env node
-
 const http = require('http');
+
+const args = process.argv.slice(2);
 const countStudents = require('./3-read_file_async');
 
-const app = http.createServer(async (req, res) => {
-  const url = req.url;
-  const databasePath = process.argv[2];
-
-  if (url === '/') {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello Holberton School!');
-  } else if (url === '/students') {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.write('This is the list of our students');
-
-    try {
-      await countStudents(databasePath)
-        .then((data) => {
-          res.end(); // End after successfully logging output
-        })
-        .catch(() => {
-          res.end('Cannot load the database\n');
-        });
-    } catch (error) {
-      res.end('Cannot load the database\n');
-    }
-  } else {
-    res.statusCode = 404;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Not Found');
-  }
-});
-
+const DATABASE = args[0];
 
 const PORT = 1245;
+
+const app = http.createServer(async (req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+
+  const { url } = req;
+
+  if (url === '/') {
+    res.write('Hello Holberton School!');
+  } else if (url === '/students') {
+    res.write('This is the list of our students\n');
+    try {
+      const students = await countStudents(DATABASE);
+      res.end(`${students.join('\n')}`);
+    } catch (error) {
+      res.end(error.message);
+    }
+  }
+  res.statusCode = 404;
+  res.end();
+});
+
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
